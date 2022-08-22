4ca7d3f2b48ff33219d2ff8abb08560b9ff5ba1e
Aleko <alekonicola@gmail.com>
2022-08-22 11:39:44 -0300

    revision front

--- TrabajoFinalFront/src/app/app.module.ts
+++ TrabajoFinalFront/src/app/app.module.ts
@@ -1,35 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { AboutComponent } from './components/about/about.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-import { ExoerienciaComponent } from './components/exoeriencia/exoeriencia.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { EditeducacionComponent } from './components/educacion/editeducacion/editeducacion.component';
-import { NeweducacionComponent } from './components/educacion/neweducacion/neweducacion.component';
-import { NewexperienciaComponent } from './components/experiencia/newexperiencia/newexperiencia.component';
-import { EditexperienciaComponent } from './components/experiencia/editexperiencia/editexperiencia.component';
-import { ModelComponent } from './model/model.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/newexperiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/editexperiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     AboutComponent,
     BannerComponent,
     EducacionComponent,
-    ExoerienciaComponent,
     ExperienciaComponent,
     FooterComponent,
     HeaderComponent,
@@ -40,15 +39,20 @@ import { ModelComponent } from './model/model.component';
     SkillsComponent,
     EditeducacionComponent,
     NeweducacionComponent,
-    NewexperienciaComponent,
-    EditexperienciaComponent,
-    ModelComponent
+    NewExperienciaComponent,
+    EditExperienciaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgCircleProgressModule.forRoot({}),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
